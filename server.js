5c0b69bf732bb338834bd851b57c0eaf22acd549
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-09-30 14:52:00 -0400

    Adds users model and controllers

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ db.on('error', (err) => console.log(err.message + ' is mongod not running?'));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
+// Middleware
+// Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Routes / Controllers
+const userController = require('./controllers/users');
+app.use('/users', userController);
+
 // LISTENER
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
