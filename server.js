3865966acae3626e537e1a8bb42b5b946d8de0e8
xcarrillo1 <Carrillo.Xavier@outlook.com>
2021-09-30 15:47:52 -0400

    Adds express session

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 // Database Configuration
 mongoose.connect(process.env.DATABASE_URL, {
@@ -19,6 +20,12 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 // Middleware
 // Body parser middleware: give us access to req.body
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
 
 // Routes / Controllers
 const userController = require('./controllers/users');
